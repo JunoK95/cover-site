f28e6754fed69c3a6cbdfa5ff18a5c42c5e7acb5
JunoK95 <junokimemail@gmail.com>
2025-03-11 11:34:18 -0600

    migrate scss @import statements to @use

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,10 +1,21 @@
+import JelloText from "../../components/JelloText/JelloText";
 import { colors } from "../../constants/colors";
 import { PageLayout } from "../../layouts/PageLayout";
+import joinClassnames from "../../utils/joinClassnames";
+import styles from "./ProjectsPage.module.scss";
+
+const leftContent = (
+  <div className={joinClassnames([styles.left, styles.centered])}>
+    <div className={styles.title}>
+      <JelloText>Projects</JelloText>
+    </div>
+  </div>
+);
 
 function ProjectsPage() {
   return (
     <PageLayout
-      leftContent={<h1>PROJECTS LEFT</h1>}
+      leftContent={leftContent}
       leftColor={colors.turquois}
       rightContent={<h1>PROJECTS RIGHT</h1>}
     />
