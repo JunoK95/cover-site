428f7108c2ad3d55cd04a3a88349f611c623845c
JunoK95 <junokimemail@gmail.com>
2025-03-14 07:43:55 -0600

    fix overflow issue and add contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,10 +1,21 @@
+import JelloText from "../../components/JelloText/JelloText";
 import { colors } from "../../constants/colors";
 import { PageLayout } from "../../layouts/PageLayout";
+import joinClassnames from "../../utils/joinClassnames";
+import styles from "./ContactPage.module.scss";
+
+const leftContent = (
+  <div className={joinClassnames([styles.left, styles.centered])}>
+    <div className={styles.title}>
+      <JelloText>Contact</JelloText>
+    </div>
+  </div>
+);
 
 function ContactPage() {
   return (
     <PageLayout
-      leftContent={<h1>CONTACT LEFT</h1>}
+      leftContent={leftContent}
       leftColor={colors.brown}
       rightContent={<h1>CONTACT RIGHT</h1>}
     />
