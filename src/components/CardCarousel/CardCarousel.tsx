db51cbbb799548d38ff983b681901dea08ee9db2
JunoK95 <junokimemail@gmail.com>
2025-04-09 08:03:43 -0600

    Add card animation and onClick handler

--- src/components/CardCarousel/CardCarousel.tsx
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselApi,
@@ -33,6 +33,14 @@ const CardCarousel = ({ cards = [] }: Props) => {
     });
   }, [api]);
 
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.scrollTo(selected);
+  }, [selected, api]);
+
   return (
     <Carousel
       opts={{
@@ -50,23 +58,38 @@ const CardCarousel = ({ cards = [] }: Props) => {
           <CarouselItem
             key={index}
             className="basis-8/12 sm:basis-1/2 lg:basis-5/12 xl:basis-4/12"
+            onClick={() => setSelected(index)}
           >
             <Card
-              className={`flex flex-col overflow-hidden h-40 transition-height duration-500 delay-200 ${
-                selected === index ? `h-60 bg-[${colors.purple}]` : ""
+              className={`flex flex-col w-full overflow-hidden shrink-0 transition-height duration-500 delay-200 select-none min-w-25 ${
+                selected === index ? styles.cardActive : styles.cardInactive
               }`}
+              style={{ padding: "0.5rem 0" }}
             >
-              <CardHeader style={{ paddingTop: "1rem" }}>
-                <CardTitle>{c.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <ul className="columns-2">
-                  {c.skills.map((skill, index) => (
-                    <li key={index} className="text-sm">
-                      {skill}
-                    </li>
-                  ))}
-                </ul>
+              <CardContent className="w-full h-full">
+                <div className="flex flex-col w-full h-full">
+                  <div
+                    id={`card-header-${index}`}
+                    className="flex justify-center place-items-center items-center"
+                    style={{ paddingBottom: "0.5rem" }}
+                  >
+                    <h3 className="text-2xl font-bold">{c.title}</h3>
+                  </div>
+                  {selected === index && (
+                    <div className="w-full">
+                      <div className="m-0 p-0 flex-row flex-wrap flex">
+                        {c.skills.map((skill, index) => (
+                          <div
+                            key={index}
+                            className="m-0 p-0 w-full max-w-[50%] box-border"
+                          >
+                            {skill}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
               </CardContent>
             </Card>
           </CarouselItem>
