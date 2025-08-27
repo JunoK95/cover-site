56a0eabd539cc171d1728bd45f7b3a1136a37649
JunoK95 <junokimemail@gmail.com>
2025-08-27 14:37:29 -0600

    Simplify HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,10 @@
-import JelloText from "../../components/JelloText/JelloText";
-import { PageLayout } from "../../layouts/PageLayout";
-import joinClassnames from "../../utils/joinClassnames";
-import styles from "./HomePage.module.scss";
-import { colors } from "../../constants/colors";
-import ChatRoom from "@/components/ChatRoom/ChatRoom";
 import { useState } from "react";
+import JelloText from "@/components/JelloText/JelloText";
+import ChatRoom from "@/components/ChatRoom/ChatRoom";
+import { PageLayout } from "@/layouts/PageLayout";
+import joinClassnames from "@/utils/joinClassnames";
+import styles from "./HomePage.module.scss";
+import { colors } from "@/constants/colors";
 import { postChat } from "@/api/postChat";
 import { postAudioChat } from "@/api/postAudioChat";
 
@@ -22,98 +22,71 @@ const initialMessages: Message[] = [
   },
 ];
 
-const leftContent = (
-  <div className={joinClassnames([styles.left, styles.centered])}>
-    <span className={styles.divDecor}>{`<div>`}</span>
-    <span className={styles.subtitle}>Hi I'm</span>
-    <div className={styles.title}>
-      <JelloText>JUNO</JelloText>
-    </div>
-    <span className={joinClassnames([styles.subtitle, styles.alignRight])}>
-      - Software Engineer
-    </span>
-    <span className={styles.divDecor}>{`</div>`}</span>
-  </div>
-);
-
-function HomePage() {
+export default function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<Message[]>(initialMessages);
 
-  const onSend = async (message: string) => {
-    console.log("onSend called with message:", message);
-    setIsLoading(true);
-    setMessages([
-      ...messages,
-      { id: messages.length + 1, text: message, sender: "right" },
-      { id: messages.length + 2, text: "Loading", sender: "left" },
-    ]);
+  const addMessage = (text: string, sender: "left" | "right") =>
+    setMessages((prev) => [...prev, { id: prev.length + 1, text, sender }]);
 
-    const receivedMessage = await postChat(message);
+  const replaceLoading = (sender: "left" | "right", newText: string) =>
     setMessages((prev) =>
       prev.map((msg) =>
-        msg.sender === "left" && msg.text === "Loading"
-          ? {
-              ...msg,
-              text: `${receivedMessage.reply}`,
-            }
+        msg.sender === sender && msg.text === "Loading"
+          ? { ...msg, text: newText }
           : msg
       )
     );
+
+  const handleSend = async (message: string) => {
+    setIsLoading(true);
+    addMessage(message, "right");
+    addMessage("Loading", "left");
+
+    const { reply } = await postChat(message);
+    replaceLoading("left", reply);
     setIsLoading(false);
   };
 
-  const onAudioSend = async (formData: FormData) => {
-    console.log("onSend called with message:", formData);
+  const handleAudioSend = async (formData: FormData) => {
     setIsLoading(true);
-    setMessages([
-      ...messages,
-      { id: messages.length + 1, text: "Loading", sender: "right" },
-      { id: messages.length + 2, text: "Loading", sender: "left" },
-    ]);
+    addMessage("Loading", "right");
+    addMessage("Loading", "left");
 
-    const receivedMessage = await postAudioChat(formData);
-    setMessages((prev) =>
-      prev.map((msg) =>
-        msg.id === messages.length + 1 && msg.sender === "right"
-          ? {
-              ...msg,
-              text: `${receivedMessage.msg}`,
-            }
-          : msg
-      )
-    );
-    setMessages((prev) =>
-      prev.map((msg) =>
-        msg.id === messages.length + 2 && msg.sender === "left"
-          ? {
-              ...msg,
-              text: `${receivedMessage.reply}`,
-            }
-          : msg
-      )
-    );
+    const { msg, reply } = await postAudioChat(formData);
+    replaceLoading("right", msg);
+    replaceLoading("left", reply);
     setIsLoading(false);
   };
-
-  const rightContent = (
-    <div className={joinClassnames([styles.right, styles.centered])}>
-      <ChatRoom
-        isLoading={isLoading}
-        messages={messages}
-        onSend={onSend}
-        onAudioSend={onAudioSend}
-      />
-    </div>
-  );
 
   return (
     <PageLayout
-      leftContent={leftContent}
       leftColor={colors.red}
-      rightContent={rightContent}
+      leftContent={
+        <div className={joinClassnames([styles.left, styles.centered])}>
+          <span className={styles.divDecor}>{`<div>`}</span>
+          <span className={styles.subtitle}>Hi I'm</span>
+          <div className={styles.title}>
+            <JelloText>JUNO</JelloText>
+          </div>
+          <span
+            className={joinClassnames([styles.subtitle, styles.alignRight])}
+          >
+            - Software Engineer
+          </span>
+          <span className={styles.divDecor}>{`</div>`}</span>
+        </div>
+      }
+      rightContent={
+        <div className={joinClassnames([styles.right, styles.centered])}>
+          <ChatRoom
+            isLoading={isLoading}
+            messages={messages}
+            onSend={handleSend}
+            onAudioSend={handleAudioSend}
+          />
+        </div>
+      }
     />
   );
 }
-
-export default HomePage;
